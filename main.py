50e662ec287f081aaf4675949a7674af7d22eba3
Drew Hund <languageinvestigator@gmail.com>
2026-05-12 13:27:59 -0400

    selenium practice

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Keep chrome open when done
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
 
+driver = webdriver.Chrome(options=chrome_options)
+# driver.get("https://www.amazon.com/dp/B075CYMYK6?ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6&th=1")
+driver.get("https://www.python.org")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+# price_dollar = driver.find_element(By.CLASS_NAME, value="a-price-whole")
+# price_cents = driver.find_element(By.CLASS_NAME, value="a-price-fraction")
+#
+# print(f"Price is {price_dollar.text}.{price_cents.text}")
 
+# search_bar = driver.find_element(By.NAME, value="q")
+# print(search_bar.get_attribute("placeholder"))
+# submit_button = driver.find_element(By.ID, value="submit")
+# print(submit_button.size)
+# doc_link = driver.find_element(By.CSS_SELECTOR, value=".documentation-widget a")
+# print(doc_link.text)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# bug_submit_link = driver.find_element(By.XPATH, value="/html/body/div/footer/div[2]/div/ul/li[3]/a")
+# print(bug_submit_link.text)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+dates = driver.find_elements(By.CSS_SELECTOR, value=".event-widget ul time")
+dates = [date.text for date in dates]
+
+events = driver.find_elements(By.CSS_SELECTOR, value=".event-widget ul a")
+events = [event.text for event in events]
+
+result = dict(zip(dates, events))
+print(result)
+
+# close active tab
+# driver.close()
+# close the whole browser
+driver.quit()
